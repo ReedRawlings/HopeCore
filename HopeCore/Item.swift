0dddcd50d7ae90ab96c9b31eb5d8924532583e47
Reed Rawlings <rawlingreed@gmail.com>
2025-11-10 10:50:07 -1000

    Initial commit

---
+++ HopeCore/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HopeCore
+//
+//  Created by Reed Rawlings on 11/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
